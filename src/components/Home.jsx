7d212bce1db27b5a7ed882702a3a719bb84139a8
maxiagui <maxigui@gmail.com>
2023-12-26 21:51:46 -0300

    agregamos la imagen del source (había quedado el link web)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import NavBar from './NavBar.jsx';
 import '../css/home.css'
+import imagBack from '../assets/images/bg-simp-fmly.png'
 
 function Home() {
   return (
@@ -8,7 +9,7 @@ function Home() {
         <NavBar/>
         <div class="dv-main-home">
             <div class="container-img-bg">
-            <img src="https://assets.stickpng.com/images/5eb95b6a17f3c600044a2905.png" alt="" />
+            <img src={imagBack} alt="" />
             </div>
             <div class="text-box-dv-home">
               <p>Explora el fascinante universo de Los Simpsons, la icónica familia amarilla que ha conquistado corazones en todo el mundo. Sumérgete en la vida de Homer, Marge, Bart, Lisa y Maggie, mientras navegas por una colección única. Descubre perfiles detallados de personajes, momentos inolvidables, y curiosidades exclusivas que hacen que Los Simpsons sean una joya cultural. Desde risas incontrolables hasta lecciones de vida, acompáñanos en este viaje lleno de humor y sátira. ¡Prepárate para reír, reflexionar y enamorarte de nuevo de la familia más amada de la televisión!</p>
